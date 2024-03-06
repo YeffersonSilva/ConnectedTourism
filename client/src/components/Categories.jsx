f795d6dd408fff76e1d2d10f1897434c338f117d
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-03-06 12:27:18 -0300

    feat: adiciona componentes de categoria ao projeto

---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,33 @@
+import { categories } from "../data";
+import "../styles/Categories.scss"
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+  return (
+    <div className="categories">
+      <h1>Explora las Categorías Destacadas</h1>
+      <p>
+        Descubre una amplia variedad de opciones que se adaptan a todos los tipos de viajeros.
+        rgete en la cultura local, disfruta de las comodidades del hogar y crea recuerdos
+        inolvidables en tu destino soñado.
+      </p>
+
+      <div className="categories_list">
+        {categories?.slice(1, 7).map((category, index) => (
+          <Link to={`/properties/category/${category.label}`}>
+            <div className="category" key={index}>
+              <img src={category.img} alt={category.label} />
+              <div className="overlay"></div>
+              <div className="category_text">
+                <div className="category_text_icon">{category.icon}</div>
+                <p>{category.label}</p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
