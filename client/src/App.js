e25b063b5ec4b08249591aa7a5a9a50b6e0dec63
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-03-08 18:10:15 -0300

    feat: adiciona funcionalidade de registro à página

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
 
 
 function App() {
@@ -9,7 +11,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
-         
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/components/MapSlide.jsx
+++ client/src/components/MapSlide.jsx
@@ -3,13 +3,21 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import '../styles/MapSlide.scss';
 
+
+// Importa la imagen de tu icono personalizado
+import markerIcon from '../assets/marker-icon.png';
+
+
 const MapSlide = () => {
   const mapRef = useRef(null);
+  const markerRef = useRef(null); // Referencia al marcador del usuario
+
 
   useEffect(() => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
         const { latitude, longitude } = position.coords;
+
 
         if (mapRef.current === null) {
           mapRef.current = L.map('map').setView([latitude, longitude], 13);
@@ -19,12 +27,42 @@ const MapSlide = () => {
         } else {
           mapRef.current.setView(new L.LatLng(latitude, longitude), 13);
         }
+
+
+        // Añadir marcador del usuario con icono personalizado
+        if (markerRef.current) {
+          markerRef.current.setLatLng([latitude, longitude]); // Actualiza la posición del marcador
+        } else {
+          const customMarkerIcon = L.icon({
+            iconUrl: markerIcon,
+            iconSize: [40, 41], // Tamaño del icono
+            iconAnchor: [12, 41] // Punto de anclaje del icono
+          });
+          markerRef.current = L.marker([latitude, longitude], { icon: customMarkerIcon }).addTo(mapRef.current);
+        }
+
+
+        // Obtener la ruta desde la ubicación del usuario a Rua Uruguai en Santana do Livramento
+        const osrmUrl = `http://router.project-osrm.org/route/v1/driving/${latitude},${longitude};-30.881944,-55.537685?overview=false`;
+        fetch(osrmUrl)
+          .then(response => response.json())
+          .then(data => {
+            const route = L.geoJSON(data.routes[0].geometry, {
+              style: {
+                color: 'red', // Cambia el color de la ruta a rojo
+                weight: 5 // Cambia el grosor de la línea de la ruta
+              }
+            }).addTo(mapRef.current);
+            mapRef.current.fitBounds(route.getBounds());
+          })
+          .catch(error => console.error("Error al obtener la ruta:", error));
       }, (error) => {
         console.error("Geolocation error: ", error);
-      });
+      }, { enableHighAccuracy: true }); // Opción para obtener una posición más precisa
     } else {
       console.log("Geolocation no es soportada por el navegador.");
     }
+
 
     return () => {
       if (mapRef.current) {
@@ -33,6 +71,7 @@ const MapSlide = () => {
       }
     };
   }, []);
+
 
   return (
     <div className="slide">
@@ -44,4 +83,7 @@ const MapSlide = () => {
   );
 };
 
+
 export default MapSlide;
+
+
