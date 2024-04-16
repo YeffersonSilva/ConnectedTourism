5feb6e84b63fdb7ade01da1df0a4b8431a60ead7
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-04-16 09:37:53 -0300

    refactor: arregla error de buscar cuidad mapa

--- client/src/components/WeatherApp.jsx
+++ client/src/components/WeatherApp.jsx
@@ -1,71 +1,63 @@
 import React, { useState, useEffect } from 'react';
-import '../styles/stylesMapaTiempo.scss'; // Asegúrate de que este es el archivo correcto
-import { useTheme } from '../contexts/ThemeContext'; // Importa useTheme
+import '../styles/stylesMapaTiempo.scss';
+import { useTheme } from '../contexts/ThemeContext';
 
 const WeatherApp = () => {
-  const [city, setCity] = useState('');
-  const [weatherData, setWeatherData] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [search, setSearch] = useState(''); // Se usa para la entrada manual del usuario
   const [mapUrl, setMapUrl] = useState('');
-  const { theme } = useTheme(); // Usa useTheme para obtener el tema actual
+  const { theme } = useTheme();
 
-  const fetchWeatherData = async (lat, lon) => {
-    const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY; // Utiliza la variable de entorno para la API key
-    let apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}&lang=pt_br`;
-
-    if (city) {
-      apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
-    }
-    try {
-      setLoading(true);
-      const response = await fetch(apiWeatherURL);
-      if (!response.ok) {
-        throw new Error('Error al obtener los datos del clima');
-      }
-      const data = await response.json();
-      setWeatherData(data);
-      setMapUrl(`https://maps.google.com/maps?q=${data.coord.lat},${data.coord.lon}&hl=es&z=14&output=embed`);
-      setError('');
-    } catch (error) {
-      setError(error.message);
-      setWeatherData(null);
-    } finally {
-      setLoading(false);
-    }
+  const updateMap = (location) => {
+    setMapUrl(`https://maps.google.com/maps?q=${encodeURIComponent(location)}&hl=es&z=14&output=embed`);
   };
 
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(position => {
-        fetchWeatherData(position.coords.latitude, position.coords.longitude);
-      }, () => {
-        setError('Geolocation is not supported by this browser.');
-      });
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const { latitude, longitude } = position.coords;
+          const location = `${latitude},${longitude}`;
+          updateMap(location); // Actualiza el mapa directamente sin mostrar la ubicación en la caja de texto
+        },
+        () => {
+          console.error('Error al acceder a la geolocalización del navegador.');
+          // Opcional: establecer una ubicación por defecto o manejar el error de otra manera
+        }
+      );
     } else {
-      setError('Geolocation is not supported by this browser.');
+      console.error('Geolocalización no soportada por este navegador.');
+      // Opcional: establecer una ubicación por defecto o manejar el error de otra manera
     }
   }, []);
 
   return (
-    <div id="weatherAppContainer" className={`container ${theme}`}> {/* Agregar clase dinámica basada en el tema */}
+    <div id="weatherAppContainer" className={`container ${theme}`}>
       <div className="form">
-        <h3>Ciudad:</h3>
+        <h3>Buscar Lugar:</h3>
         <div className="form-input-container">
           <input
             type="text"
-            placeholder="Nombre de la ciudad"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            onKeyPress={(e) => e.key === 'Enter' && fetchWeatherData(null, null)}
+            placeholder="Introduce ubicación"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyPress={(e) => e.key === 'Enter' && updateMap(search)}
           />
-          <button onClick={() => fetchWeatherData(null, null)}>
+          <button onClick={() => updateMap(search)}>
             Buscar
           </button>
         </div>
       </div>
       <div id="mapbox">
-        <iframe className="iframe-border" width="350" height="250" frameBorder="0" scrolling="no" marginHeight="0" marginWidth="0" src={mapUrl}></iframe>
+        <iframe
+          className="iframe-border"
+          width="350"
+          height="250"
+          frameBorder="0"
+          scrolling="no"
+          marginHeight="0"
+          marginWidth="0"
+          src={mapUrl}
+        ></iframe>
       </div>
     </div>
   );
