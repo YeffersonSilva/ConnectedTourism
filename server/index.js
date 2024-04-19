a35be676c38888ab94e7d7754143df3cc9672730
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-04-19 17:43:02 -0300

    feat: initialize server and integrate Swagger for API documentation

--- server/index.js
+++ server/index.js
@@ -1,35 +1,42 @@
+// Importación de módulos necesarios
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 
+// Configuración de la aplicación Express
+const app = express();
+app.use(cors());
+app.use(express.json()); // Para parsear application/json
+app.use(express.static("public")); // Para servir archivos estáticos
+
+// Importación de rutas
 const authRoutes = require("./routes/auth.js");
 const listingRoutes = require("./routes/listing.js");
 const bookingRoutes = require("./routes/booking.js");
 const userRoutes = require("./routes/user.js");
-const eventRoutes = require("./routes/eventRoutes.js"); // Importa las rutas de eventos
+const eventRoutes = require("./routes/eventRoutes.js");
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
-
-/* ROUTES */
+// Configuración de rutas
 app.use("/auth", authRoutes);
 app.use("/properties", listingRoutes);
 app.use("/bookings", bookingRoutes);
 app.use("/users", userRoutes);
-app.use("/events", eventRoutes); // Usa las rutas de eventos con un prefijo
+app.use("/events", eventRoutes);
 
-/* MONGOOSE SETUP */
+const setupSwagger = require("./routes/swagger.js");
+setupSwagger(app);  /* MONGOOSE SETUP */
 const PORT = 3001;
-mongoose
-  .connect(process.env.MONGO_URL, {
-    dbName: "Dream_Nest",
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-  })
-  .catch((err) => console.log(`${err} did not connect`));
+// MongoDB setup
+mongoose.connect(process.env.MONGO_URL, { dbName: "Dream_Nest" })
+    .then(() => {
+        console.log("Connected to MongoDB");
+        // Inicio del servidor una vez establecida la conexión a la base de datos
+        const PORT = process.env.PORT || 3001;
+        app.listen(PORT, () => {
+            console.log(`Server running on port: ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error: ", err);
+    });
