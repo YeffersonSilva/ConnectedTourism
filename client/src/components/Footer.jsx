4ac388c6ff755c42166fb5a1ad698f516e812392
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-04-24 13:34:47 -0300

    feat: add visual effect to footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -16,27 +16,31 @@ const Footer = () => {
         <a href="/"><img src="/assets/logo01.png" alt="logo" /></a>
       </div>
 
+      {/* Efecto del planeta Tierra */}
+      <div className="earth"></div>
+
       <div className="footer_center">
         {/* Aquí puedes colocar contenido adicional si es necesario */}
       </div>
 
       <div className="footer_right">
+        {/* Enlace a la documentación del backend */}
+        <div className="footer_right_info">
+          <img src="/assets/api-documentation.png" alt="API Documentation Icon" style={{ width: '24px', height: '24px', marginRight: '5px' }} />
+          <a href="http://localhost:3001/api-docs/" target="_blank">Documentación ApiRest</a>
+        </div>
         <h3>Contact</h3>
         <div className="footer_right_info" onClick={toggleChat}>
           <img src="/icons8-charla.gif" alt="Chat Icon" style={{ cursor: 'pointer', width: '24px', height: '24px' }} />
           <p>Chat para ayuda al Usuario</p>
-</div>
-
-         
-
-
-<div className="footer_right_info">
-    <img src="/telegram.png" alt="Telegram Icon" style={{ width: '24px', height: '24px', marginRight: '5px' }} />
-    <a href="https://t.me/NaviExplore_bot" target="_blank">Contacto Telegram</a>
-</div>
-
-        {isChatOpen && <Contenido />} {/* Muestra el chatbot cuando se activa */}
+        </div>
+        <div className="footer_right_info">
+          <img src="/telegram.png" alt="Telegram Icon" style={{ width: '24px', height: '24px', marginRight: '5px' }} />
+          <a href="https://t.me/NaviExplore_bot" target="_blank">Contacto Telegram</a>
+        </div>
       </div>
+
+      {isChatOpen && <Contenido />} {/* Muestra el chatbot cuando se activa */}
     </div>
   );
 }
