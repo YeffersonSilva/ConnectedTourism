809b9402e88e493f6f26431debfa7ee77892253d
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-22 15:19:25 -0300

    style: add animation to category headings in Categories component

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { categories } from "../data";
 import "../styles/Categories.scss";
 import { Link } from "react-router-dom";
 import { useTheme } from '../contexts/ThemeContext'; // Importa useTheme
+import { motion } from 'framer-motion'; // Importa motion de framer-motion
 
 const Categories = () => {
   const { theme } = useTheme(); // Usa useTheme para obtener el tema actual
+  const [animateKey, setAnimateKey] = useState(0);
+
+  useEffect(() => {
+    setAnimateKey(prevKey => prevKey + 1);
+  }, []);
 
   return (
     <div className={`categories ${theme}`}> {/* Agrega la clase de tema aquí */}
-      <h1>Explora las Categorías Destacadas</h1>
+      <motion.h1
+        key={animateKey}
+        initial={{ opacity: 0, y: -50 }} // Empieza arriba
+        animate={{ opacity: 1, y: 0 }} // Termina en su posición original
+        transition={{ duration: 0.9}}
+      >
+        Explora las Categorías Destacadas
+      </motion.h1>
       <div className="categories_list">
         {categories?.slice(1, 7).map((category, index) => (
           <Link to={`/properties/category/${category.label}`} key={index}>
-            <div className="category">
+            <motion.div
+              key={animateKey}
+              className="category"
+              initial={{ opacity: 0, x: -50 }} // Empieza a la izquierda
+              animate={{ opacity: 1, x: 0 }} // Termina en su posición original
+              transition={{ duration: 0.6, delay: index * 0.4 }}
+            >
               <img src={category.img} alt={category.label} />
               <div className="overlay"></div>
               <div className="category_text">
                 <div className="category_text_icon">{category.icon}</div>
                 <p>{category.label}</p>
               </div>
-            </div>
+            </motion.div>
           </Link>
         ))}
       </div>
