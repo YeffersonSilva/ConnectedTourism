bd459d8d1907a113a8a2eddc5910d011d3490e48
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-07-09 22:31:09 -0300

    refactor: update login page background image and admin page styles
    The code changes update the background image URL in the LoginPage component to use a local image file instead of a remote URL. Additionally, the admin page styles have been refactored to improve the layout and spacing of various elements.
    
    These changes enhance the visual appeal and user experience of the login page and admin section.

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -8,8 +8,7 @@ const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   // Actualiza aquí la URL de Unsplash para usar 'Santana do Livramento' como palabra clave
-  const backgroundImageUrl = "https://source.unsplash.com/1600x900/?España";
-
+  const backgroundImageUrl = "/assets/Rivera01.jpg";
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
